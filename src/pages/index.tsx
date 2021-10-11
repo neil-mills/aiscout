688d5a4474c6ce3e08be09fd651e84cde4b7f962
Neil Mills <mail@neil-mills.co.uk>
2021-10-11 15:19:18 +0100

    Add prop for small img to parallax component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,9 @@ import ImageBannerParallax from '../components/ImageBannerParallax'
 import News from '../components/News'
 import Organisations from '../components/Organisations'
 import ParallaxImg1 from '../assets/images/paralax-img-1.jpg'
+import ParallaxImg1Sm from '../assets/images/parallax-img-1-sm.jpg'
 import ParallaxImg2 from '../assets/images/paralax-img-2.jpg'
+import ParallaxImg2Sm from '../assets/images/parallax-img-2-sm.jpg'
 import Instagram from '../components/Instagram'
 import HexBg from '../assets/images/hex-bg.png'
 
@@ -34,14 +36,19 @@ const HomePage = ({ data }) => {
         </FeatureBlockWrapper>
         <Stats />
         <ImageBannerParallax
-          img={ParallaxImg1}
+          imgLg={ParallaxImg1}
+          imgSm={ParallaxImg1Sm}
           heading={'An equal opportunity for all amateur players.'}
           text={'All you need is a mobile phone.'}
           icon={true}
         />
         <News />
         <Organisations />
-        <ImageBannerParallax img={ParallaxImg2} logo={true} />
+        <ImageBannerParallax
+          imgLg={ParallaxImg2}
+          imgSm={ParallaxImg2Sm}
+          logo={true}
+        />
         <Instagram data={data} />
       </div>
     </>
